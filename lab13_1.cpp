deb8db45f662bbd7e988652a345af08b967a8567
Nunnalin Bingbing <61963924+snnmbb@users.noreply.github.com>
2021-01-22 17:07:14 +0700

    finished

--- lab13_1.cpp
+++ lab13_1.cpp
@@ -3,8 +3,27 @@ using namespace std;
 
 template <typename T>
 void insertionSort(T d[],int N){
-
+	int j;
+	T key;
+	for( int i = 1 ;i<N ;i++)
+	{
+		cout<<"Pass "<<i<<":";
+		key = d[i];
+		j = i-1;
+		while(j>=0&&d[j]<key)
+		{
+			d[j+1] = d[j];
+			j = j-1;
+		}
+		d[j+1]=key;
+		for(int i = 0;i<N;i++)
+		{
+			cout<<d[i]<<" ";
+		}	
+		cout<<"\n";
+	}
 }
+
 
 int main(){
 	int a[10] = {12,25,30,44,2,0,4,7,55,25};
@@ -17,3 +36,7 @@ int main(){
 	cout << "\nSorted Array:";
 	for(int i = 0; i < 10; i++) cout << a[i] << " ";	
 }
+
+
+
+  
